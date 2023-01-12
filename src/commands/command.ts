9f800ed3bf07931ad6823ef8a04246906b8186b8
Jay <84829249+Dolsity@users.noreply.github.com>
2023-01-11 22:55:16 -0800

    Update command.ts

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -1,6 +1,6 @@
 import discord from 'discord.js'
 
 export default {
-  builder: new discord.SlashCommandBuilder().setName('name').setDescription('description'),
+  builder: new discord.SlashCommandBuilder().setName('NAME').setDescription('DESCRIPTION'),
   execute: (async (interaction: discord.CommandInteraction) => {})
 }
